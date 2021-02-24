56b1db59ed0c894f9242dcbecca3415ed4beb5de
Cristian Rusu <cristian.rusu@unibuc.ro>
2021-02-24 19:14:23 +0200

    primul commit al proiectului
    un proiect simplu, de test

---
+++ program.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int binarySearch(int arr[], int l, int r, int x)
+{
+    if (r >= l)
+	{
+        int mid = (l + r) / 2;
+
+        if (arr[mid] == x)
+            return mid;
+
+        if (arr[mid] > x)
+            return binarySearch(arr, l, mid - 1, x);
+
+        return binarySearch(arr, mid + 1, r, x);
+    }
+
+    return -1;
+}
+
+int main()
+{
+    int arr[] = {1, 2, 3, 10, 20, 30, 40, 50};
+
+    int n = sizeof(arr) / sizeof(arr[0]);
+	
+    int x = 40;
+
+    int rezultat = binarySearch(arr, 0, n - 1, x);
+
+	if (rezultat == -1)
+		printf("Elementul nu este in array");
+	else
+        printf("Element este in array la index %d", rezultat);
+
+    return 0;
+}
